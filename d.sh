ce476ff8e870d3bff32d1fb2a626cdb221f7639a
anilkumar <anilozilramsey@gmail.com>
2026-01-22 17:20:19 +0530

    neww

--- d.sh
+++ d.sh
@@ -3,7 +3,7 @@ THRESHOLD=90
 
 diskusage=$(df -h | grep /dev/nvme0n1p3 | awk '{print $5}' | cut -d'%' -f1)
 
-if ["$diskusage -gt $THRESHOLD"] then
+if ["$diskusage -gt $THRESHOLD"]; then
    echo"diskusage is low (${diskusage}%"
 else
    echo"diskusage is high (${diskusage}%"
