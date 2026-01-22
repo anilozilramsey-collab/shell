8e5f6e7d8e4b3917afabc98a29c65e2c1439fb2f
anilkumar <anilozilramsey@gmail.com>
2026-01-22 16:39:26 +0530

    newsyntaxadded

--- diskusage.sh
+++ diskusage.sh
@@ -1,12 +1,11 @@
 #!/bash/bin
 THRESHOLD=90
 
-diskusage=$(df -h | grep /dev/nvme0n1p3 | awk '{print $5}| cut %
-
+diskusage=$ (df -h | grep /dev/nvme0n1p3 | awk '{print $5}| cut %) 
 
 if [$diskusage -gt $THRESHOLD]
    print'diskusage is low'
 else
    print'diskusage is high'
-   
+
 fi
